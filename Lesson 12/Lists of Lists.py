bb4790a8f66602b2798b9331797844af75d52245
Jessie Tang <tangqiaojie@hotmail.com>
2017-07-13 20:44:06 -0700

    added comments about lists of lists

--- Lesson 12/Lists of Lists.py
+++ Lesson 12/Lists of Lists.py
@@ -53,4 +53,12 @@ print total_enrollment(udacious_univs)
 
 print total_enrollment(usa_univs)
 #>>> (77285,3058581079)
-    
+
+
+# 1.确定需要return是什么
+# 2.
+# 因为是需要不断把上面一个数值累积。return得到的是一个total。初始时候，从上面累积下来是0，什么也没有。所以，要定义一个初始值（0）。f
+# or e in p 中，e是每次需要从每一个list抓取出来的。是一个变量！
+# 3. e 是一个list 里面可以是很多元素，这些元素也是可以为lists，（lists of lists）所以在for e in p
+# 里面可以用 list of list 中几个元素表示
+
